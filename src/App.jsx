e120648477ffa650e8b0913c2f3f7d821716da90
Vlad <vlad_np@ukr.net>
2025-05-21 10:20:32 +0200

    renovate  BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
-import { Routes, Route, Link, Router } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Main from "./Pages/Main";
 import UserArray from "./Pages/UserArray";
 import "./App.css";
 
 function App() {
   return (
-    <Router basename="/HOMY-LIMITED">
-      <div className="mainContainer">
-        <nav className="nav">
-          <Link to="/">Головна</Link>
-          <Link to="/user_array">Робота з даними</Link>
-        </nav>
+    <div className="mainContainer">
+      <nav className="nav">
+        <Link to="/">Головна</Link>
+        <Link to="/user_array">Робота з даними</Link>
+      </nav>
 
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/user_array" element={<UserArray />} />
-        </Routes>
-      </div>
-    </Router>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/user_array" element={<UserArray />} />
+      </Routes>
+    </div>
   );
 }
 
