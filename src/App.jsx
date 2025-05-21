7dd99d52a640ca6588e5e3233b07d911e4b0be8f
Vlad <vlad_np@ukr.net>
2025-05-21 10:06:10 +0200

    add basename

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, Router } from "react-router-dom";
 import Main from "./Pages/Main";
 import UserArray from "./Pages/UserArray";
 import "./App.css";
 
 function App() {
   return (
-    <div className="mainContainer">
-      <nav className="nav">
-        <Link to="/">Головна</Link>
-        <Link to="/user_array">Робота з даними</Link>
-      </nav>
+    <Router basename="/HOMY-LIMITED">
+      <div className="mainContainer">
+        <nav className="nav">
+          <Link to="/">Головна</Link>
+          <Link to="/user_array">Робота з даними</Link>
+        </nav>
 
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/user_array" element={<UserArray />} />
-      </Routes>
-    </div>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/user_array" element={<UserArray />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
